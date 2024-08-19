230f3ac66a0601a37bbffcfbdfb1389841e96d48
Ben Chan <bengood362@gmail.com>
2024-08-19 16:55:20 +0800

    fix: updated script to print top N processes

--- print_top_usage_process.py
+++ print_top_usage_process.py
@@ -6,29 +6,31 @@ import subprocess
 # USER         PID %CPU %MEM    VSZ   RSS TTY      STAT START   TIME COMMAND
 # uuuuser+     737  0.0  0.0   2836   992 ?        Ss    2023   0:00 command
 # uuuuser+     744  0.0  4.4 2397260 346668 ?      Sl    2023  54:02 command
-
-ps_regexp_fmt = '(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)'
+top_process_count = 4
+ps_regexp_fmt = '(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(.+)'
 process_to_print = {}
 
 stats_to_sort = ['pcpu', 'rss']
 for stats in stats_to_sort:
   proc = subprocess.Popen(['ps', 'ux', '--sort=pcpu'], stdout=subprocess.PIPE)
   line = proc.stdout.readline() # skip first line
+  count = 0
   while True:
-    line = proc.stdout.readline()
-    if not line:
+    line = proc.stdout.readline().decode('ascii')
+    if not line or count > top_process_count:
       break
-    re_search_result = re.search(ps_regexp_fmt, str(line))
+    re_search_result = re.search(ps_regexp_fmt, line)
     if not re_search_result:
       continue;
     user, pid, cpu_percent, mem_percent, vsz, rss, tty, stat, start, time, command = re_search_result.groups()
     process_to_print[pid] = {
-      'command': command,
-      'cpu_perc': cpu_percent,
-      'mem_perc': mem_percent,
-      'vsz': vsz,
-      'rss': rss,
+      'command': command.strip(),
+      'cpu_perc': cpu_percent.strip(),
+      'mem_perc': mem_percent.strip(),
+      'vsz': vsz.strip(),
+      'rss': rss.strip(),
     }
+    count += 1
 
 for pid in process_to_print:
   process_metadata = process_to_print[pid]
