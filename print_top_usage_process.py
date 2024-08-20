ce8446177e9fdb001972ad998d92c683f362370b
Ben Chan <bengood362@gmail.com>
2024-08-20 15:23:52 +0800

    feat: added sanitization for label value, updated print criteria

--- print_top_usage_process.py
+++ print_top_usage_process.py
@@ -8,11 +8,23 @@ import subprocess
 # uuuuser+     744  0.0  4.4 2397260 346668 ?      Sl    2023  54:02 command
 top_process_count = 4
 ps_regexp_fmt = '(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(.+)'
+command_prefix_to_replace = 'cmd_'
+pid_prefix_to_replace = 'pid_'
 process_to_print = {}
 
-stats_to_sort = ['pcpu', 'rss']
+def sanitize_label_value(string, prefix_to_replace):
+  # label_value must match [a-zA-Z_][a-zA-Z0-9_]*
+  # https://prometheus.io/docs/concepts/data_model/
+  (first_char, *rest_chars) = string
+  if not re.match('[a-zA-Z_]', first_char):
+    rest_chars = [first_char, *rest_chars]
+    first_char = prefix_to_replace
+  rest_chars = list(map(lambda c: c if re.match('[a-zA-Z0-9_]', c) else '_', rest_chars))
+  return ''.join([first_char, *rest_chars])
+
+stats_to_sort = ['-pcpu', '-rss']
 for stats in stats_to_sort:
-  proc = subprocess.Popen(['ps', 'ux', '--sort=pcpu'], stdout=subprocess.PIPE)
+  proc = subprocess.Popen(['ps', 'ux', '--sort={}'.format(stats)], stdout=subprocess.PIPE)
   line = proc.stdout.readline() # skip first line
   count = 0
   while True:
@@ -24,7 +36,7 @@ for stats in stats_to_sort:
       continue;
     user, pid, cpu_percent, mem_percent, vsz, rss, tty, stat, start, time, command = re_search_result.groups()
     process_to_print[pid] = {
-      'command': command.strip(),
+      'command': command.strip().split(' ')[0],
       'cpu_perc': cpu_percent.strip(),
       'mem_perc': mem_percent.strip(),
       'vsz': vsz.strip(),
@@ -35,9 +47,34 @@ for stats in stats_to_sort:
 for pid in process_to_print:
   process_metadata = process_to_print[pid]
   command = process_metadata['command']
-  print('cpu_perc{{pid="{pid}",command="{command}"}} {value}'.format(command=command, pid=pid, value=process_metadata['cpu_perc']))
-  print('mem_perc{{pid="{pid}",command="{command}"}} {value}'.format(command=command, pid=pid, value=process_metadata['mem_perc']))
-  print('vsz_usage{{pid="{pid}",command="{command}"}} {value}'.format(command=command, pid=pid, value=process_metadata['vsz']))
-  print('rss_usage{{pid="{pid}",command="{command}"}} {value}'.format(command=command, pid=pid, value=process_metadata['rss']))
+  cpu_perc = process_metadata['cpu_perc']
+  mem_perc = process_metadata['mem_perc']
+  vsz = process_metadata['vsz']
+  rss = process_metadata['rss']
+
+  if cpu_perc > 0:
+    print('cpu_perc{{pid="{pid}", command="{command}"}} {value}'.format(
+      command=sanitize_label_value(command, command_prefix_to_replace),
+      pid=sanitize_label_value(pid, pid_prefix_to_replace),
+      value=cpu_perc,
+    ))
+  if mem_perc > 0:
+    print('mem_perc{{pid="{pid}", command="{command}"}} {value}'.format(
+      command=sanitize_label_value(command, command_prefix_to_replace),
+      pid=sanitize_label_value(pid, pid_prefix_to_replace),
+      value=mem_perc,
+    ))
+  if vsz > 0:
+    print('vsz_usage{{pid="{pid}", command="{command}"}} {value}'.format(
+      command=sanitize_label_value(command, command_prefix_to_replace),
+      pid=sanitize_label_value(pid, pid_prefix_to_replace),
+      value=vsz,
+    ))
+  if rss > 0:
+    print('rss_usage{{pid="{pid}", command="{command}"}} {value}'.format(
+      command=sanitize_label_value(command, command_prefix_to_replace),
+      pid=sanitize_label_value(pid, pid_prefix_to_replace),
+      value=rss,
+    ))
   print()
 
